45faba5d6a4472b45f9e4bab5e9dc09e224daba6
kayeeesan <eijansantosdominique13@gmail.com>
2020-02-23 10:09:13 +0800

    Added client side

---
+++ Project-client/src/views/Dashboard.jsx
@@ -0,0 +1,76 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line, Pie } from "react-chartjs-2";
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col
+} from "reactstrap";
+// core components
+import {
+  dashboard24HoursPerformanceChart,
+  dashboardEmailStatisticsChart,
+  dashboardNASDAQChart
+} from "variables/charts.jsx";
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+         
+          <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Users Behavior</CardTitle>
+                  <p className="card-category">24 Hours performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboard24HoursPerformanceChart.data}
+                    options={dashboard24HoursPerformanceChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-history" /> Updated 3 minutes ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
