d56eecb87c016a16805fae33ebb8d2d021f087c6
kayeeesan <eijansantosdominique13@gmail.com>
2020-03-07 22:03:23 +0800

    Setting's updated

--- Project-client/src/components/SettingCards/SettingCards.jsx
+++ Project-client/src/components/SettingCards/SettingCards.jsx
@@ -23,7 +23,7 @@ class SettingCards extends React.Component {
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-single-02 text-primary" />
+                                            <i className="nc-icon nc-single-02" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
@@ -49,7 +49,7 @@ class SettingCards extends React.Component {
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-money-coins text-success" />
+                                            <i className="nc-icon nc-money-coins" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
@@ -75,12 +75,143 @@ class SettingCards extends React.Component {
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-bell-55 text-primary" />
+                                            <i className="nc-icon nc-bell-55" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
                                         <div className="numbers">
                                             <p className="card-category">Notification</p>
+                                            <CardTitle tag="p">0</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-align-left-2" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Course</p>
+                                            <CardTitle tag="p">0</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-align-left-2" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Department</p>
+                                            <CardTitle tag="p">0</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-badge" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Student</p>
+                                            <CardTitle tag="p">0</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-badge" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Employee</p>
+                                            <CardTitle tag="p">0</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-planet" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Medicine</p>
                                             <CardTitle tag="p">0</CardTitle>
                                             <p />
                                         </div>
